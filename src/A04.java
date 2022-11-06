c61cab48ffbf740d69c721d24f6f979e467d3b12
Emre idi <emreidi@hotmail.com>
2022-11-06 19:07:46 +0300

    aciklamalar eklendi

--- src/A04.java
+++ src/A04.java
@@ -1,5 +1,19 @@
 public class A04 {
     public static void main(String[] args) {
 
+
+        /*
+        bir projeyi ilk defa olusturgdugumuzda
+        1- commit olustur
+        2- uzak masaustunde bir repo olustur
+        3- github'da istenen son 3 satiri copy yapip
+           projeye past yapip calistir
+
+           Bir projeyi bir kere uzak masaustu ile iliskilendirince
+           sonra yapacagim her degisilik icin
+           1- git add . : yeni yapilan degisiklikleri git'e ekler
+           2- git commit -m"istenen aciklama" : degisilikleri isleyerek versiyon olusturur
+           3- git push : son versiyonu uzak masaustune yollar
+         */
     }
 }
